a2181177b7695995ea495fc9515f4bae5925f6ac
Jet <fiero19892@gmail.com>
2022-06-24 16:07:39 +0800

    Fix add function

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 let addToDoBtnElem = document.querySelector("#toDoBtn");
 let todoContainerElem = document.querySelector("#todoContainer");
-let inputFieldElem = document.querySelector("#inputField")
+let inputFieldElem = document.querySelector("#inputField") 
 
-addToDoBtnElem.addEventListener("click", function(){
+addToDoBtnElem.addEventListener("click", function(){ 
     var task = document.createElement('p');
+    todoContainerElem.appendChild(task);
     task.classList.add("task");
     task.textContent = inputFieldElem.value;
     inputFieldElem.value = "";
